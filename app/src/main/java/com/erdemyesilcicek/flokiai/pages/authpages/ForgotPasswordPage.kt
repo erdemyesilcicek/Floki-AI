58ace3b96b617e67cc111d2f6afc6b20e030a684
erdemyesilcicek <erdmyslcck9@outlook.com>
2025-01-20 21:26:33 +0300

    reset password is over. :)

--- app/src/main/java/com/erdemyesilcicek/flokiai/pages/authpages/ForgotPasswordPage.kt
+++ app/src/main/java/com/erdemyesilcicek/flokiai/pages/authpages/ForgotPasswordPage.kt
@@ -1,8 +1,17 @@
 package com.erdemyesilcicek.flokiai.pages.authpages
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,16 +23,26 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.erdemyesilcicek.flokiai.R
+import com.erdemyesilcicek.flokiai.animations.LottieAnimation
 import com.erdemyesilcicek.flokiai.auth.AuthViewModel
 import com.erdemyesilcicek.flokiai.auth.AuthViewModelFactory
 import com.erdemyesilcicek.flokiai.components.CustomAlertDialog
+import com.erdemyesilcicek.flokiai.components.CustomExtendedFAB
+import com.erdemyesilcicek.flokiai.components.CustomTextButton
 import com.erdemyesilcicek.flokiai.components.CustomTextInput
+import com.erdemyesilcicek.flokiai.utils.myFont
 
 @Composable
 fun ForgotPasswordPage(
@@ -32,33 +51,83 @@ fun ForgotPasswordPage(
 ) {
     val email = remember { mutableStateOf("") }
     val message = remember { mutableStateOf("") }
+    val scrollState = rememberScrollState()
     var alertDialogActive by remember { mutableStateOf<Boolean>(false) }
 
     Column(
         modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+            .fillMaxSize()
+            .imePadding()
+            .verticalScroll(scrollState)
+            .background(Color.White),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        CustomTextInput(
-            title = stringResource(id = R.string.title_email),
-            label = stringResource(id = R.string.label_email),
-            text = email.value,
-            onValueChange = { email.value = it },
-            isSingleLine = true,
-            isVisual = true,
-            keyboardType = KeyboardType.Email,
-            isBigCanvas = false
-        )
-        if (message.value.isNotEmpty()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            LottieAnimation(animation = R.raw.security)
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
             Text(
-                message.value,
-                color = Color.Red,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp),
+                text = "Reset your password",
+                lineHeight = 42.sp,
+                textAlign = TextAlign.Start,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = myFont,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp),
+                text = "Please enter your email, reset link will be sent to you.",
+                textAlign = TextAlign.Start,
                 fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = myFont,
+                color = MaterialTheme.colorScheme.onPrimary
             )
         }
-        Button(
-            onClick = {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CustomTextInput(
+                title = stringResource(id = R.string.title_email),
+                label = stringResource(id = R.string.label_email),
+                text = email.value,
+                onValueChange = { email.value = it },
+                isSingleLine = true,
+                isVisual = true,
+                keyboardType = KeyboardType.Email,
+                isBigCanvas = false
+            )
+            if (message.value.isNotEmpty()) {
+                Text(
+                    message.value,
+                    color = Color.Red,
+                    fontSize = 16.sp,
+                )
+            }
+
+            CustomExtendedFAB(
+                containerColor = MaterialTheme.colorScheme.primary,
+                text = "Reset Password",
+            ) {
                 viewModel.resetPassword(
                     email.value,
                     onSuccess = {
@@ -68,17 +137,19 @@ fun ForgotPasswordPage(
                         message.value = it
                     }
                 )
-            }) {
-            Text(
-                text = "RESET PASSWORD",
-                fontSize = 16.sp
-            )
+            }
+            CustomTextButton(
+                text = stringResource(id = R.string.already_have_an_account),
+                color = MaterialTheme.colorScheme.onBackground
+            ) {
+                navController.navigate("SignInPage")
+            }
         }
     }
     if (alertDialogActive) {
         CustomAlertDialog(
             title = "Password Reset",
-            message = "Password reset link has been sent to your email address.",
+            message = "Reset link sent to your email.",
             buttonText = "OK",
             buttonColor = MaterialTheme.colorScheme.primary,
             onButtonClick = {
--- app/src/main/java/com/erdemyesilcicek/flokiai/pages/authpages/SignInPage.kt
+++ app/src/main/java/com/erdemyesilcicek/flokiai/pages/authpages/SignInPage.kt
@@ -11,21 +11,16 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -44,7 +39,6 @@ import com.erdemyesilcicek.flokiai.components.CustomTextButton
 import com.erdemyesilcicek.flokiai.components.CustomTextInput
 import com.erdemyesilcicek.flokiai.utils.myFont
 import com.erdemyesilcicek.flokiai.viewmodels.UserInformationViewModel
-import kotlinx.coroutines.launch
 
 @Composable
 fun SignInPage(
